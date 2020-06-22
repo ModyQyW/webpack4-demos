3c4707ba16363b16d9b4c51367f02884ef96863d
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-06-22 10:39:48 +0800

    style: format demo01

--- demo01/src/index.js
+++ demo01/src/index.js
@@ -1,1 +1,1 @@
-document.write("Hello webpack!");
+document.write('Hello webpack!');
--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -1,20 +1,20 @@
 // use path module
-const path = require("path");
+const path = require('path');
 // require plugins
-const { CleanWebpackPlugin: CleanPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const HtmlPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin: CleanPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 const WebpackBar = require('webpackbar');
 
 module.exports = {
   // mode
-  mode: "production",
+  mode: 'production',
   // entry
-  entry: path.resolve(__dirname, "src", "index.js"),
+  entry: path.resolve('src', 'index.js'),
   // output
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
   },
   // plugins
   plugins: [
@@ -24,12 +24,12 @@ module.exports = {
     new CleanPlugin(),
     // copy ./public/favicon.ico
     new CopyPlugin({
-      patterns: [{ from: path.resolve(__dirname, "public", "favicon.ico") }],
+      patterns: [{ from: path.resolve('public', 'favicon.ico') }],
     }),
     // use ./public/index.html as template
     new HtmlPlugin({
-      title: "demo01",
-      template: path.resolve(__dirname, "public", "index.html"),
+      title: 'demo01',
+      template: path.resolve('public', 'index.html'),
     }),
   ],
 };
