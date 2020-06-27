8f60776b7458d8135100ae1aa0173ce31af2f043
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-06-27 13:23:56 +0800

    feat(demo02): add webpack-bundle-analyzer

--- demo02/config/webpack.prod.js
+++ demo02/config/webpack.prod.js
@@ -1,7 +1,15 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(baseConfig, {
   mode: 'production',
   devtool: 'none',
+  plugins: [
+    // analyze bundle
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      defaultSizes: 'stat',
+    }),
+  ],
 });
