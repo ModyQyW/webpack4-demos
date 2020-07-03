21e27e950e9e7e79044861f3f7ced9043d0e24aa
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-07-03 12:00:23 +0800

    feat: use promise to test polyfill

--- demo03/src/index.js
+++ demo03/src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from 'zent';
 import iconWebpack from './assets/webpack.png';
 import './index.scss';
 
-const App = () => (
-  <div className="container">
-    <p>Hello Webpack!</p>
-    <img src={iconWebpack} />
-    <Button type="primary">Hello Zent!</Button>
-  </div>
-);
+const App = () => {
+  useEffect(() => {
+    new Promise((resolve) => {
+      setTimeout(() => {
+        document.title = 'Hello World!';
+        resolve();
+      }, 5000);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <p>Hello Webpack!</p>
+      <img src={iconWebpack} />
+      <Button type="primary">Hello Zent!</Button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
