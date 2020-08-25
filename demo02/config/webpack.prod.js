aa164e210bd971347c1140fa286f36b1cc458047
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-08-25 17:21:46 +0800

    build: remove webpack-bundle-analyzer in demo02

--- demo02/config/webpack.prod.js
+++ demo02/config/webpack.prod.js
@@ -1,16 +1,7 @@
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(baseConfig, {
   mode: 'production',
   devtool: 'cheap-source-map',
-  plugins: [
-    // analyze bundle
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      defaultSizes: 'stat',
-      openAnalyzer: false,
-    }),
-  ],
 });
