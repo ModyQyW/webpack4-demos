bd7b854631c7e2512eace596a14a355e60352a2d
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-11-23 23:17:32 +0800

    update demo02

---
+++ demo02/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import {
+  LayoutRow as Row,
+  LayoutCol as Col,
+  LayoutGrid as Grid,
+  Button,
+  Icon,
+} from 'zent';
+import iconWebpack from './assets/webpack.png';
+import './App.scss';
+
+const App = () => {
+  useEffect(() => {
+    new Promise((resolve) => {
+      setTimeout(() => {
+        document.title = 'Hello World!';
+        resolve();
+      }, 5000);
+    });
+  }, []);
+
+  return (
+    <Grid id="app">
+      <Row>
+        <Col span={24}>
+          <img className="icon" src={iconWebpack} />
+          <Button type="primary">
+            <Icon type="youzan" />
+            Hello Zent!
+          </Button>
+        </Col>
+      </Row>
+    </Grid>
+  );
+};
+
+export default App;
--- demo02/src/index.js
+++ demo02/src/index.js
@@ -1,29 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Icon } from 'zent';
-import iconWebpack from './assets/webpack.png';
+import App from './App.jsx';
 import './index.scss';
 
-const App = () => {
-  useEffect(() => {
-    new Promise((resolve) => {
-      setTimeout(() => {
-        document.title = 'Hello World!';
-        resolve();
-      }, 5000);
-    });
-  }, []);
-
-  return (
-    <div className="container">
-      <p>Hello Webpack!</p>
-      <img src={iconWebpack} />
-      <Button type="primary">
-        <Icon type="youzan" />
-        Hello Zent!
-      </Button>
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
